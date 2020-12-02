f412cb88ba50d3a25a67778595601dcc6eb8a2a3
fruityloops1 <fruityloops@littun.co>
2020-12-02 20:01:44 +0100

    Invalid Argument Message

--- Messages.cs
+++ Messages.cs
@@ -5,6 +5,7 @@ namespace BitCronch
         public class English
         {
             public static string NoArgs = "Error: No arguments provided";
+            public static string InvalidArgs = "Error: Invalid arguments";
             public static string Usage = "Usage: bitcronch <file>\n" +
                                          "  -o <output file>";
         }
